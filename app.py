ec8d07e5fd473122de00a9af03261d244ce6524e
hasanpagla <devhasanvibes@gmail.com>
2025-05-27 11:05:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -125,6 +125,3 @@ def matrix_result():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
